fee062fff56bbb19797ce813d372f6954fc4fab1
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-06-05 13:58:12 -0800

    fix Eagle Village latlon

--- geocode communities.R
+++ geocode communities.R
@@ -4,7 +4,8 @@ pce <- read.csv("data/power-cost-equalization-pce-data.csv")
  
 latlon <- geocode(paste0(levels(pce$community_names), ", ", "Alaska") )
 latlon <- cbind(community_names = levels(pce$community_names), latlon)
-
-write.csv(latlon, file = "latlon.csv")
+latlon[latlon$community_names == "Eagle, Eagle Village",2:3] <- c(-141.114722, 64.781389)
+write.csv(latlon, file = "latlon.csv", row.names = FALSE)
 
 ggplot(latlon, aes(x = lon, y = lat, label = community_names)) + geom_text()
+#Does anything look wierd? It should not. 
