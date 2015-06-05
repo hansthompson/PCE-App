ae0c5d0c39071b3e3c1c48229181665c1838057f
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-06-05 13:03:46 -0800

    added quandl data import.
    also start of geocoding script.

--- analysis.R
+++ analysis.R
@@ -1,13 +1,13 @@
+#install_github('quandl/R-package')
+library(Quandl)
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(ggmap)
 #read in data
 pce <- read.csv("data/power-cost-equalization-pce-data.csv")
 
-dim(pce)
-summary(pce)
-
-pce<- select(pce, community_names, plant__name, plant__utility__name,
+pce<- select(pce, community_names, plant__name, plant__utility__name, plant__akeps_region__name,
        year, month, intertied, residential_rate, pce_rate, pro_rata_rate,
        effective_rate, fuel_price, fuel_used_gal, fuel_cost, nonfuel_expenses,
        diesel_efficiency, diesel_kwh_generated, peak_consumption_kw, 
@@ -16,15 +16,32 @@ pce<- select(pce, community_names, plant__name, plant__utility__name,
        community_customers, government_customers, unbilled_customers, 
        other_customers)
 
-pce$time <- ymd(paste(pce$year, pce$month, "01"))
-
-ggplot(data = filter(pce, community_names == "Sleetmute"), aes(x = time, y = peak_consumption_kw)) + geom_line()
+pce$Date <- as.Date(ymd(paste(pce$year, pce$month, "01")))
+#Remove empty factor level for region
+pce <- filter(pce, plant__akeps_region__name != "")
 
 summarized_pce <- pce %>% 
-    group_by(time) %>%
+    group_by(Date) %>%
     summarize(sum = mean(fuel_price, na.rm = T))
 
-ggplot(summarized_pce, aes(x = time, y = sum)) + geom_line()
+ggplot(summarized_pce, aes(x = Date, y = sum)) + geom_line()
 
+#Diesel Fuel Refiner Wholesale Price, Monthly
+#https://www.quandl.com/data/EIA/STEO_DSWHUUS_M-Diesel-Fuel-Refiner-Wholesale-Price-Monthly 
+#Only 50 requests a day. 
+wholesale_fuel <- Quandl("EIA/STEO_DSWHUUS_M")
+#add a day. This offsets the time of the price by a day. 
+wholesale_fuel$Date <- as.Date(wholesale_fuel$Date + days(1) )
+#convert to dolllars from cents
+wholesale_fuel$Value <- wholesale_fuel$Value / 100
 
+combined_price <- inner_join(wholesale_fuel, pce, by = "Date")
 
+#Patterns of price inflation by geographic region
+ggplot(data = combined_price, aes(x = fuel_price, y = Value, color = plant__akeps_region__name, alpha = 0.1)) + geom_point() + coord_fixed() + geom_smooth(method = "lm")
+#distribution of cost difference from wholesale and fuel_price to village
+ggplot(data = combined_price, aes(x = fuel_price - Value, fill = plant__akeps_region__name)) + geom_histogram()
+#change in difference over time
+ggplot(data = combined_price, aes(x = factor(Date), y = fuel_price - Value)) + geom_boxplot() + facet_wrap(~plant__akeps_region__name) + ggtitle("From 07/2001-06/2013")
+#
+ggplot(data = combined_price, aes(x = ))
