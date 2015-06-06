7069eb6ca839ba810a4cc6e9b4c5db0252d5b13a
Charles Hans Thompson <hans.thompson1@gmail.com>
2015-06-05 16:47:23 -0800

    join data with geocoded villages

--- analysis.R
+++ analysis.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(ggmap)
 #read in data
 pce <- read.csv("data/power-cost-equalization-pce-data.csv")
+latlon <- read.csv("latlon.csv")
 
 pce<- select(pce, community_names, plant__name, plant__utility__name, plant__akeps_region__name,
        year, month, intertied, residential_rate, pce_rate, pro_rata_rate,
@@ -15,6 +16,8 @@ pce<- select(pce, community_names, plant__name, plant__utility__name, plant__ake
        government_kwh_sold, unbilled_kwh, residential_customers, commercial_customers, 
        community_customers, government_customers, unbilled_customers, 
        other_customers)
+
+pce <- inner_join(latlon, pce, by = "community_names")
 
 pce$Date <- as.Date(ymd(paste(pce$year, pce$month, "01")))
 #Remove empty factor level for region
@@ -37,11 +40,16 @@ wholesale_fuel$Value <- wholesale_fuel$Value / 100
 
 combined_price <- inner_join(wholesale_fuel, pce, by = "Date")
 
+
 #Patterns of price inflation by geographic region
 ggplot(data = combined_price, aes(x = fuel_price, y = Value, color = plant__akeps_region__name, alpha = 0.1)) + geom_point() + coord_fixed() + geom_smooth(method = "lm")
 #distribution of cost difference from wholesale and fuel_price to village
 ggplot(data = combined_price, aes(x = fuel_price - Value, fill = plant__akeps_region__name)) + geom_histogram()
 #change in difference over time
 ggplot(data = combined_price, aes(x = factor(Date), y = fuel_price - Value)) + geom_boxplot() + facet_wrap(~plant__akeps_region__name) + ggtitle("From 07/2001-06/2013")
-#
-ggplot(data = combined_price, aes(x = ))
+#show 
+most_recent <- filter(combined_price, Date == "2013-06-01")
+ggplot(data = most_recent, aes(x = lon, y = lat, label = community_names, color = fuel_price)) + geom_text() + scale_colour_gradientn(colours=rainbow(4))
+ggplot(data = most_recent, aes(x = lon, y = lat, label = community_names, color = fuel_cost) ) + geom_text() + scale_colour_gradientn(colours=rainbow(4))
+ggplot(data = most_recent, aes(x = lon, y = lat, label = community_names, color = residential_customers)) + geom_text() + scale_colour_gradientn(colours=rainbow(4))
+mean(most_recent$fuel_used_gal, na.rm = T)
